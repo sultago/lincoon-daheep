a6e83da0fe8c92404b897229946e01621da75c34
Park, Da Hee <dahepark@ebay.com>
2019-05-22 13:41:59 +0900

    이미지 변경 - 이것저것 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo-apeach.jpg';
 import './App.css';
 
-const App: React.FC = () => {
+const App: React.FC = () => {     // function component
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="App-logo" alt="apeach logo" />
       </header>
+      <div>
+        test
+      </div>
     </div>
   );
 }
